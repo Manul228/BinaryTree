a4a9e7fa3975992b9b701fa8a664aef8639b70a0
unknown <gkornilov8@gmail.com>
2021-12-13 18:06:43 +0300

    isSymmetric

--- binarytree.go
+++ binarytree.go
@@ -82,6 +82,97 @@ func postorderTraversal(root *TreeNode) []int {
 	return travers
 }
 
+func levelOrder(root *TreeNode) [][]int {
+	var levels [][]int
+
+	if root == nil {
+		return levels
+	}
+
+	var queue []*TreeNode
+	var leaves []*TreeNode
+	var node *TreeNode
+	var level []int
+
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		node = queue[0]
+		queue = queue[1:]
+
+		level = append(level, node.Val)
+
+		if node.Left != nil {
+			leaves = append(leaves, node.Left)
+		}
+
+		if node.Right != nil {
+			leaves = append(leaves, node.Right)
+		}
+
+		if len(queue) == 0 {
+			levels = append(levels, level)
+			level = make([]int, 0)
+
+			for _, l := range leaves {
+				queue = append(queue, l)
+			}
+
+			leaves = make([]*TreeNode, 0)
+		}
+	}
+
+	return levels
+}
+
+func helperMaxDepth(root *TreeNode, depth int, answer *int) {
+	if root == nil {
+		if depth > *answer {
+			*answer = depth
+		}
+		return
+	}
+
+	helperMaxDepth(root.Left, depth+1, answer)
+	helperMaxDepth(root.Right, depth+1, answer)
+
+	return
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	var answer int
+
+	helperMaxDepth(root, 0, &answer)
+
+	return answer
+}
+
+func isSymmetric(root *TreeNode) bool {
+	var leftTraverse [][]int = levelOrder(root.Left)
+	var rightTraverse [][]int = levelOrder(root.Right)
+
+	if len(leftTraverse) != len(rightTraverse) {
+		return false
+	}
+
+	fmt.Println(leftTraverse)
+	fmt.Println(rightTraverse)
+
+	for i, lt := range leftTraverse {
+		for j, rt := range rightTraverse[i] {
+			if lt[len(lt)-j-1] != rt {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
 func main() {
 	root := TreeNode{}
 	root.Val = 1
@@ -90,12 +181,30 @@ func main() {
 	left.Val = 2
 
 	right := TreeNode{}
-	right.Val = 3
+	right.Val = 2
+
+	// left1 := TreeNode{}
+	// left1.Val = 3
+
+	right1 := TreeNode{}
+	right1.Val = 4
+
+	left2 := TreeNode{}
+	left2.Val = 4
+
+	// right2 := TreeNode{}
+	// right2.Val = 3
 
 	root.Left = &left
 	root.Right = &right
 
-	result := postorderTraversal(&root)
+	// root.Left.Left = &left1
+	root.Left.Right = &right1
+
+	root.Right.Left = &left2
+	// root.Right.Right = &right2
+
+	result := isSymmetric(&root)
 
 	fmt.Println(result)
 }
